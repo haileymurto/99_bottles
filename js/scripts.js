621d3b31acfec1bf16f7b9e1b0fcaecf588c0893
Hailey Murto and Matt Phillips <hailey.murto@gmail.com>
2016-02-15 20:04:04 -0800

    makes site using looping with for.

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+
+  for (var index = 10; index >= 0; index -= 1) {
+    alert(index + ' bottles of beer on the wall' + index + ' bottles of beer. Take one down, pass it around.' + index + ' bottles of beer on the wall');
+  }
